886b750d4fd111041d52c6330fc94763c42c143e
Visar Domi <visardomi4@gmail.com>
2019-05-30 00:44:13 +0200

    reservation CRUD

--- api/helper_functions/decorators.py
+++ api/helper_functions/decorators.py
@@ -1,7 +1,6 @@
 from flask import g
 from functools import wraps
-from ..common.exceptions import YouAreNotAdmin, CannotChangeOthersData
-from .common_function import can_it_update
+from ..common.exceptions import YouAreNotAdmin
 
 
 def admin_required(a_function):
@@ -14,18 +13,3 @@ def admin_required(a_function):
             raise YouAreNotAdmin(message=msg)
 
     return decorated_function
-
-
-def can_make_reservation(employee_id=0, company_id=0, driver_id=0):
-    def real_decorator(a_function):
-        @wraps(a_function)
-        def decorated_function(*args, **kwargs):
-            can_update = can_it_update(employee_id, company_id, driver_id)
-            if can_update:
-                return a_function(*args, **kwargs)  # here goes the function
-            else:
-                msg = "You can't change other people's data."
-                raise CannotChangeOthersData(message=msg)
-
-        return decorated_function
-    return real_decorator
